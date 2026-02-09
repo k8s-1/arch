7b18f85108d5d9d4d7b86f35c6c74fd5350b309a
k8s-1 <176627561+k8s-1@users.noreply.github.com>
2026-02-09 23:36:47 +0100

    chore: update file(s): audit.sh

--- arch-dotfiles/audit.sh
+++ arch-dotfiles/audit.sh
@@ -5,3 +5,13 @@ sudo lynis audit system
 sudo arch-audit
 
 sudo systemd-analzye security
+
+
+# rkhunter
+# Prior to running for the first time, update file properties db:
+rkhunter --propupd
+
+# Running:
+rkhunter --update
+rkhunter --check --sk
+rkhunter --config-check
